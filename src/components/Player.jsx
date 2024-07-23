82e85608c66d6ec2fb1f01ea4ed419b513328a6c
K0unty <kountiapi@pm.me>
2024-07-23 20:01:34 +0530

    useREf

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState("");
+  // useref
+  const playerName = useRef();
 
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
 
   function handleClick() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome 💋 {submitted ? enteredPlayerName : "👅PuusyLiqr"}</h2>
+      <h2>Welcome 💋 {enteredPlayerName ?? "👅PuusyLiqr"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
