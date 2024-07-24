c7f4b465a75ed307e3f336bab02d47110de5d3f6
K0unty <kountiapi@pm.me>
2024-07-24 13:38:04 +0530

    FixTImer

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemaningTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -16,14 +19,14 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>😡Lost😡</h2>}
       <p>
         TargetTime = <strong>{targetTime}s. </strong>
       </p>
       <p>
-        Stopped Timer <strong>X seconds left</strong>
+        Stopped Timer <strong> {formattedRemaningTime} seconds left</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
